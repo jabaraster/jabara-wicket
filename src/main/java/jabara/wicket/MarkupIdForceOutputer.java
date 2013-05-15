b53690b8bd160a58504638a59d7a080e69be9167
jabaraster <jabaraster@gmail.com>
2013-05-16 01:11:04 +0900

    #1

--- src/main/java/jabara/wicket/MarkupIdForceOutputer.java
+++ src/main/java/jabara/wicket/MarkupIdForceOutputer.java
@@ -5,6 +5,7 @@ package jabara.wicket;
 
 import org.apache.wicket.Component;
 import org.apache.wicket.application.IComponentInstantiationListener;
+import org.apache.wicket.markup.html.TransparentWebMarkupContainer;
 
 /**
  * @author jabaraster
@@ -16,6 +17,10 @@ public class MarkupIdForceOutputer implements IComponentInstantiationListener {
      */
     @Override
     public void onInstantiation(final Component pComponent) {
+        if (pComponent.getClass().equals(TransparentWebMarkupContainer.class)) {
+            // TransparentWebMarkupContainerのid属性を出力すると警告が出るので、これは除外.
+            return;
+        }
         pComponent.setOutputMarkupId(true);
     }
 
