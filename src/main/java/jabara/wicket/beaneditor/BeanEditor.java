78a32d1a5e871da257c35aeddb0eb282d66bb3c6
Jabara <jabaraster@gmail.com>
2013-06-05 16:22:03 +0900

    mainte.

--- src/main/java/jabara/wicket/beaneditor/BeanEditor.java
+++ src/main/java/jabara/wicket/beaneditor/BeanEditor.java
@@ -13,7 +13,7 @@ import org.apache.wicket.markup.html.list.LoopItem;
 import org.apache.wicket.markup.html.panel.Panel;
 
 /**
- * @param <E>
+ * @param <E> 編集対象オブジェクトの型.
  * @author jabaraster
  */
 public class BeanEditor<E> extends Panel {
@@ -27,18 +27,18 @@ public class BeanEditor<E> extends Panel {
     /**
      * 
      */
-    protected final BeanProperties propertiesModel;
+    protected final BeanProperties propertiesValue;
 
     private Loop                   properties;
 
     /**
-     * @param pId
-     * @param pBean
+     * @param pId -
+     * @param pBean -
      */
     public BeanEditor(final String pId, final E pBean) {
         super(pId);
         this.bean = ArgUtil.checkNull(pBean, "pBean"); //$NON-NLS-1$
-        this.propertiesModel = BeanProperties.getInstance(pBean.getClass());
+        this.propertiesValue = BeanProperties.getInstance(pBean.getClass()).toVisiblePropertiesOnly();
         this.add(getProperties());
     }
 
@@ -51,12 +51,12 @@ public class BeanEditor<E> extends Panel {
 
     private Loop getProperties() {
         if (this.properties == null) {
-            this.properties = new Loop("properties", this.propertiesModel.size()) { //$NON-NLS-1$
+            this.properties = new Loop("properties", this.propertiesValue.size()) { //$NON-NLS-1$
                 private static final long serialVersionUID = 8600810849579410319L;
 
                 @Override
                 protected void populateItem(final LoopItem pItem) {
-                    final BeanProperty property = BeanEditor.this.propertiesModel.get(pItem.getIndex());
+                    final BeanProperty property = BeanEditor.this.propertiesValue.get(pItem.getIndex());
                     pItem.add(new Label("label", property.getLocalizedName())); //$NON-NLS-1$
                     pItem.add(new PropertyEditor<E>("property", BeanEditor.this.bean, property)); //$NON-NLS-1$
                 }
