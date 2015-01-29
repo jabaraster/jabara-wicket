a9045a84cea693b00861529f8d9bf8c7a56246c5
jabaraster <jabaraster@gmail.com>
2015-01-29 21:51:18 +0900

    mainte.

--- src/main/java/jabara/wicket/Components.java
+++ src/main/java/jabara/wicket/Components.java
@@ -4,6 +4,7 @@
 package jabara.wicket;
 
 import jabara.general.ArgUtil;
+import jabara.general.ExceptionUtil;
 import jabara.general.NotFound;
 
 import org.apache.wicket.Component;
@@ -26,9 +27,14 @@ public final class Components {
      * @return -
      * @throws NotFound -
      */
-    public static <C extends Component> C findFirst(final MarkupContainer pRoot, final Class<C> pFindTarget) throws NotFound {
+    public static <C extends Component> C findFirst( //
+            final MarkupContainer pRoot //
+            , final Class<C> pFindTarget //
+    ) throws NotFound {
+
         ArgUtil.checkNull(pRoot, "pRoot"); //$NON-NLS-1$
         ArgUtil.checkNull(pFindTarget, "pFindTarget"); //$NON-NLS-1$
+
         final C ret = pRoot.visitChildren(pFindTarget, new IVisitor<C, C>() {
             @Override
             public void component(final C pObject, final IVisit<C> pVisit) {
@@ -39,6 +45,25 @@ public final class Components {
             throw NotFound.GLOBAL;
         }
         return ret;
+    }
+
+    /**
+     * @param pRoot -
+     * @param pFindTarget -
+     * @return -
+     */
+    public static <C extends Component> C findFirstUnsafe( //
+            final MarkupContainer pRoot //
+            , final Class<C> pFindTarget //
+    ) {
+        ArgUtil.checkNull(pRoot, "pRoot"); //$NON-NLS-1$
+        ArgUtil.checkNull(pFindTarget, "pFindTarget"); //$NON-NLS-1$
+
+        try {
+            return findFirst(pRoot, pFindTarget);
+        } catch (final NotFound e) {
+            throw ExceptionUtil.rethrow(e);
+        }
     }
 
 }
